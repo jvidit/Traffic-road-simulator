f969802c35650fe7aea8cae7a5483b5cbd9ff195
vidit jain <jvidit5@gmail.com>
2019-03-04 15:55:10 +0530

    Header changed

--- Traffic road sim/Event.h
+++ Traffic road sim/Event.h
@@ -1,15 +1,10 @@
 
-//Design Changes made:
-//1. Combined "move to position" and "create vehicle" in the same event, as no "move to position" events are made during simulation
-//2. Rather than generating things(i.e., road, traffic light and vehicle) based on the event in the main, Event itself generates the instances of objects
-
 
 class Event
 {
 
 	//useful for debugging
-	String description;
-
+    String description;
 
     int eventType;
     // 0 - create road
@@ -26,8 +21,8 @@ class Event
     
     //params for road
     static int defaultRoadId;                                                             //ID of road
-    static int defaultRoadlength;                                                         //length of road, default = 100
-    static int defaultRoadwidth;                                                          //width of road, default = 5
+    static int defaultRoadlength;                                                         //length of road
+    static int defaultRoadwidth;                                                          //width of road
     static trafficLight defaultRoadTl;           
 
     //params for traffic light
@@ -56,30 +51,9 @@ public:
 	//depending on type of event, allocates memory to road, traffic light or vehicle
 	Event(int timestamp,String message);
 
-
-	int eventType;
-    	// 0 - create road
-  	// 1 - create traffic light
-  	// 2 - create vehicle and mov to position P
-   
-    	int timestamp;
-
-	public:
-  	Road road;
-  	TrafficLight trafficLight;
-  	Vehicle vehicle;
-	
-
-	//depending on type of event, allocates memory to road, traffic light or vehicle
-	Event(int timestamp,String description);
-     	/*IDEA - We dont need to set default parameters in our other classes, we can pass parameters from here by using static variables*/
-
 	String getDescription();
 
-   int getTimeStamp();
-
-	String getDescription()
-	{	return description;		}
+    int getTimeStamp();
 
 	
 }
