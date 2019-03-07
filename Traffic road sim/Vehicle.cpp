32556b9026797593a2f79cc955c3e504dab1652b
vidit jain <jvidit5@gmail.com>
2019-03-07 16:33:39 +0530

    Added basic logic for acceleration. Corrected minor errors

--- Traffic road sim/Vehicle.cpp
+++ Traffic road sim/Vehicle.cpp
@@ -43,7 +43,7 @@
 	{
 		
 		bool canMove=true;
-		for(int i=position.upPos;i<=position.downPos;ji+)
+		for(int i=position.upPos;i<=position.upPos+length;i++)
 		{
 			if(positionArr[i][position.rightPos+1]!='_')
 			{
